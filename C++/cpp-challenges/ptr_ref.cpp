4677d7245ee3facb4d3c7453c2d71c9984b13549
HawSJun <ghkdtjrwns12@naver.com>
2025-03-26 07:40:11 +0900

    C++ 복습!

---
+++ C++/cpp-challenges/ptr_ref.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+// [1] C++ 참조 방식
+void SwapByReference(int* (&ptr1), int* (&ptr2)) {
+    int* temp = ptr1;
+    ptr1 = ptr2;
+    ptr2 = temp;
+}
+
+// [2] C 스타일 포인터 방식
+void SwapByPointer(int** ptr1, int** ptr2) {
+    int* temp = *ptr1;
+    *ptr1 = *ptr2;
+    *ptr2 = temp;
+}
+
+int main() {
+
+    int a = 100;
+    int b = 200;
+
+    int* ptr1 = &a;
+    int* ptr2 = &b;
+
+    cout << "=== Before Swap ===" << endl;
+    cout << "*ptr1: " << *ptr1 << ", *ptr2: " << *ptr2 << endl;
+
+    // === 참조 방식 호출 ===
+    SwapByReference(ptr1, ptr2);            // 참조로 포인터 자체가 바뀜
+
+    cout << "\n=== After SwapByReference ===" << endl;
+    cout << "*ptr1: " << *ptr1 << ", *ptr2: " << *ptr2 << endl;
+
+    // 다시 원래대로 바꾸기
+    SwapByPointer(&ptr1, &ptr2);            // 참조로 포인터 자체가 바뀜
+
+    cout << "\n=== After SwapByPointer ===" << endl;
+    cout << "*ptr1: " << *ptr1 << ", *ptr2: " << *ptr2 << endl;
+
+    return 0;
+}
